6263b6d55ff8fb7d34a8823cdaa7daa1d6b2d9de
Abhishek Bisen <bisenabhishek65@gmail.com>
2023-07-20 00:51:33 +0530

    real time object detection app

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:object_detection/realtime/live_camera.dart';
+import 'package:object_detection/static%20image/static.dart';
+import 'package:object_detection/SideBar/NavBar.dart';
+
+List<CameraDescription> cameras;
+
+Future<void> main() async {
+  // initialize the cameras when the app starts
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  // running the app
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData.dark(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavBar(),
+      appBar: AppBar(
+        title: Text("Object Detector App"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.info),
+            onPressed: aboutDialog,
+          ),
+        ],
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ButtonTheme(
+                minWidth: 170,
+                child: ElevatedButton(
+                  child: Text("Detect in Image"),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => StaticImage(),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              ButtonTheme(
+                minWidth: 160,
+                child: ElevatedButton(
+                  child: Text("Real Time Detection"),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LiveFeed(cameras),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  aboutDialog() {
+    showAboutDialog(
+      context: context,
+      applicationName: "Object Detector App",
+      applicationLegalese: "By Abhishek Bisen",
+      applicationVersion: "1.0",
+      children: <Widget>[
+        Text("bisenabhishek65@gmail.com"),
+      ],
+    );
+  }
+}
